89e7b93da321d39d9ffc46cd9b339d001a7feb6d
Anthony <anthony@futureworkz.com>
2015-03-11 11:58:59 +0700

    Seed create 100 vendors

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,22 @@ unless admin
       password: '123123123'
   )
 end
+
+puts '==Creating vendor =='
+vendors = Vendor.all
+if vendors.count < 100
+  (1..100).each do |i|
+    Vendor.create(
+        name: "Vendor #{i}",
+        email: "vendor#{i}@example.com",
+        password: '123123123',
+        address:  'Somewhere in Singapore',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        phone: '6652-3568',
+        fax:  '6652-3578',
+        picture: '',
+        longitude: '103.8492009',
+        latitude: '1.3233667',
+    )
+  end
+end
