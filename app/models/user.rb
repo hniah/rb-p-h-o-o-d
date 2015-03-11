33d42b8fd80b4aa7a1d837e153446d9d024cd675
Anthony <anthony@futureworkz.com>
2015-03-11 13:24:54 +0700

    Set default picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  DEFAULT_URL = '/images/users/picture/:style/missing.jpg'
-  PATH = '/public/:class/:attachment/:id/:style_:basename.:extension'
+  DEFAULT_URL = ''
+  PATH = ':rails_root/public/:class/:attachment/:id/:style_:basename.:extension'
   VALIDATE_SIZE = { in: 0..1.megabytes, message: 'Photo size too large. Please limit to 1 mb.' }
   has_attached_file :picture,
                     styles: {thumb: '210x210#', large: '600x600#'},
