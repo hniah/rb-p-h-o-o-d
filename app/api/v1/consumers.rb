dcd92f9d7e810e50be9eaf29c9e4f0e5b046feaa
Anthony <anthony@futureworkz.com>
2015-03-13 11:44:47 +0700

    Complete post /api/v1/consumer/social

--- app/api/v1/consumers.rb
+++ app/api/v1/consumers.rb
@@ -9,18 +9,55 @@ module V1
       end
 
       post do
-        consumer = Consumer.new({
-            name:params[:name],
-            email:params[:email],
-            device_token:params[:device_token],
-            platform:params[:platform]
-        })
-        if consumer.save
-          ApiHelper.response(200, 'Consumer has inserted successfully')
+        consumer = Consumer.find_by_device_token(params[:device_token])
+        if consumer.present?
+          ApiHelper.response(200, 'Consumer has been created before', consumer)
         else
-          ApiHelper.response(500, 'Consumer has inserted unsuccessfully')
+          consumer = Consumer.new({
+              name:params[:name],
+              email:params[:email],
+              device_token:params[:device_token],
+              platform:params[:platform]
+          })
+          if consumer.save
+            ApiHelper.response(200, 'Consumer was inserted successfully', consumer)
+          else
+            ApiHelper.response(500, 'Consumer was inserted unsuccessfully')
+          end
         end
       end
+
+
+      desc 'Store consumer social post'
+
+      params do
+        requires :consumer_id, type:String
+        requires :vendor_id, type:String
+        requires :message, type:String
+        requires :social_type, type:String
+      end
+
+      post 'social' do
+
+        post = Post.new({
+            consumer_id:  params[:consumer_id],
+            vendor_id:    params[:vendor_id],
+            message:      params[:message],
+            picture:      params[:picture],
+            social_type:  params[:social_type]
+        })
+
+        if post.save
+          data_vendor = Vendor.find(params[:vendor_id]).data_vendor
+          data_vendor = data_vendor.present? ? ApiHelper.filter_reward_vendor(data_vendor) : nil
+
+          ApiHelper.response(200, 'Post was inserted successfully', data_vendor)
+        else
+          ApiHelper.response(500, 'Post was inserted unsuccessfully')
+        end
+
+      end
+
     end
   end
 end
--- spec/api/v1/consumer_spec.rb
+++ spec/api/v1/consumer_spec.rb
@@ -3,12 +3,34 @@ require 'rails_helper'
 RSpec.describe V1::Consumers do
   describe 'POST /consumer' do
     let(:error_code) { JSON.parse(response.body)['error_code'] }
+    let(:consumer) { create :consumer}
+    context 'Consumer does exited' do
+      it 'return error_code is 200' do
+        post '/api/v1/consumer', {api_key: ENV['API_KEY'], name: consumer.name, email: consumer.email, device_token:consumer.device_token, platform: consumer.platform}
+        expect(Consumer.first.email).to eq consumer.email
+        expect(error_code).to eq 200
+      end
+    end
 
-    it 'return error_code is 200' do
-      post '/api/v1/consumer?api_key='+ENV['API_KEY']+'&name=Anthony&email=consumer@example.com&device_token=837362&platform=android'
+    context 'Consumer does not exited' do
+      it 'return error_code is 200' do
+        post '/api/v1/consumer', {api_key: ENV['API_KEY'], name: 'Anthony', email: 'consumer@example.com', device_token:32132, platform: 'android'}
+        expect(Consumer.first.email).to eq 'consumer@example.com'
+        expect(error_code).to eq 200
+      end
+    end
+  end
 
-      expect(Consumer.first.email).to eq 'consumer@example.com'
-      expect(error_code).to eq 200
+  describe 'POST /social' do
+    let(:reward) { JSON.parse(response.body)['data'] }
+    let!(:consumer) { create :consumer}
+    let!(:vendor) { create :vendor}
+
+    it 'return reward of vendor' do
+      post '/api/v1/consumer/social', {api_key: ENV['API_KEY'], consumer_id: consumer.id, vendor_id: vendor.id, message: 'Testing post', social_type: :facebook}
+
+      expect(Post.first.message).to eq 'Testing post'
+      expect(reward['reward_detail']).to eq vendor.data_vendor.reward_detail
     end
   end
 end
