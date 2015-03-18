befefba687498fdcdb13f8ed35d84e60b8c87ce7
Anthony <anthony@futureworkz.com>
2015-03-18 16:25:44 +0700

    Complete vendor update info feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def home
 
@@ -12,6 +13,13 @@ class ApplicationController < ActionController::Base
       rails_admin_path
     else
       root_path
+    end
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :name , :description, :address, :phone, :fax, :longitude, :latitude, :picture, :password_confirmation, :current_password)
     end
   end
 end
