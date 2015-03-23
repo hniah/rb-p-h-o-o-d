f06f45ee1d9a59d3a30e60eab4db000a432e5038
Anthony <anthony@futureworkz.com>
2015-03-23 13:34:37 +0700

    Add spec for dashboard

--- spec/controllers/vendors/vendors_controller_spec.rb
+++ spec/controllers/vendors/vendors_controller_spec.rb
@@ -5,14 +5,26 @@ describe VendorsController do
   let(:vendor) { create :vendor}
   let(:consumer) { create :consumer}
   let!(:post) {Post.create(consumer_id:consumer.id, vendor_id: vendor.id, message: 'I like this food', social_type: 'facebook')}
-
   before do
     sign_in vendor
   end
 
+  describe '#GET show vendor info' do
+    def do_request
+      get :show
+    end
+
+    it 'render vendor info' do
+      do_request
+      expect(response).to render_template :dashboard
+      expect(assigns(:total_sharing)).to eq 1
+      expect(assigns(:vendor)).to_not be_nil
+    end
+  end
+
   describe '#GET consumer_sharing' do
     def do_request
-      get :sharing, id: vendor.id
+      get :sharing
     end
 
     it 'render list of consumer sharing' do
