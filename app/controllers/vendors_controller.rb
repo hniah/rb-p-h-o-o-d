ca383e4f6a81f266ec4777b8273026b7e67e8752
Anthony <anthony@futureworkz.com>
2015-03-25 13:56:16 +0700

    Redirect admin to dashboard

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,5 +1,5 @@
 class VendorsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :admin_can_not_access
 
   def show
     @vendor = current_user
@@ -10,5 +10,12 @@ class VendorsController < ApplicationController
 
   def sharing
     @shares = Post.where(vendor_id: current_user.id).page params[:page]
+  end
+
+  private
+  def admin_can_not_access
+    if current_user.is_a?(Admin)
+      redirect_to rails_admin_path
+    end
   end
 end
