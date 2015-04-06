3a05af1e015e53a5f125affc5d39cdd3febd42e6
Anthony <anthony@futureworkz.com>
2015-04-06 09:56:54 +0700

    Remove insert consumer api

--- app/api/v1/consumers.rb
+++ app/api/v1/consumers.rb
@@ -1,33 +1,6 @@
 module V1
   class Consumers < Grape::API
     resource :consumer do
-      desc 'Create a new consumer'
-
-      params do
-        requires :email, type:String
-        requires :device_token, type:String
-      end
-
-      post do
-        consumer = Consumer.find_by_device_token(params[:device_token])
-        if consumer.present?
-          ApiHelper.response(200, 'Consumer has been created before', consumer)
-        else
-          consumer = Consumer.new({
-              name:params[:name],
-              email:params[:email],
-              device_token:params[:device_token],
-              platform:params[:platform]
-          })
-          if consumer.save
-            ApiHelper.response(200, 'Consumer was inserted successfully', consumer)
-          else
-            ApiHelper.response(500, 'Consumer was inserted unsuccessfully')
-          end
-        end
-      end
-
-
       desc 'Store consumer social post'
 
       params do
--- spec/api/v1/consumer_spec.rb
+++ spec/api/v1/consumer_spec.rb
@@ -1,25 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe V1::Consumers do
-  describe 'POST /consumer' do
-    let(:error_code) { JSON.parse(response.body)['error_code'] }
-    let(:consumer) { create :consumer}
-    context 'Consumer does exited' do
-      it 'return error_code is 200' do
-        post '/api/v1/consumer', {api_key: ENV['API_KEY'], name: consumer.name, email: consumer.email, device_token:consumer.device_token, platform: consumer.platform}
-        expect(Consumer.first.email).to eq consumer.email
-        expect(error_code).to eq 200
-      end
-    end
-
-    context 'Consumer does not exited' do
-      it 'return error_code is 200' do
-        post '/api/v1/consumer', {api_key: ENV['API_KEY'], name: 'Anthony', email: 'consumer@example.com', device_token:32132, platform: 'android'}
-        expect(Consumer.first.email).to eq 'consumer@example.com'
-        expect(error_code).to eq 200
-      end
-    end
-  end
 
   describe 'POST /social' do
     let(:reward) { JSON.parse(response.body)['data'] }
