0bafbe3f3c999ad969269769fc12314364305187
Anthony <anthony@futureworkz.com>
2015-04-14 14:00:39 +0700

    Show all fields of consumer in rails_admin

--- app/models/concerns/rails_admin/consumer.rb
+++ app/models/concerns/rails_admin/consumer.rb
@@ -3,27 +3,7 @@ module Concerns::RailsAdmin::Consumer
 
   included do
     rails_admin do
-      edit do
-        field :email
-        field :password
-        field :password_confirmation
-        field :name
-        field :phone
-      end
 
-      list do
-        field :id
-        field :email
-        field :name
-        field :phone
-      end
-
-      show do
-        field :id
-        field :email
-        field :name
-        field :phone
-      end
     end
   end
 end
--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,4 +1,7 @@
 class Consumer < ActiveRecord::Base
+  extend Enumerize
   has_many :posts, dependent: :destroy
   validates :device_token,  presence: true, uniqueness: true
+
+  enumerize :gender, in: [:male, :female], default: :male
 end
